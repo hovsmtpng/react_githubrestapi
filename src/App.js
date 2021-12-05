fd6d67242cc81e5fc5e95adb447a5e86d451bc9c
Hovely Wahyu <siburiansimatupang98@gmail.com>
2021-12-05 17:26:56 +0700

    pre-development (setting react, add material ui for design template)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+
+      </header> */}
+      <div className="App-container">
+        <h1>Mini Project - Github Rest API</h1>
+        <h2>Hovely Simatupang</h2>
+        <h2>hovelywzsimatupang@gmail.com</h2>
+        <h2>082216601511</h2>
+        <button>Go !!!</button>
+      </div>
     </div>
   );
 }
