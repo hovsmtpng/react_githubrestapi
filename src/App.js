d44d7151c837abdf197097d31ae6430fad59d1ce
Hovely Wahyu <siburiansimatupang98@gmail.com>
2021-12-06 16:00:02 +0700

    add axios to get data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import React, { useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import axios from 'axios';
 
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
@@ -14,114 +15,109 @@ import { Topbar } from "./Styles/";
 
 
 function App() {
-  const [user, setUser] = useState({
-    profile: {},
-  });
+  const [user, setUser] = useState({});
+  const [repository, setRepository] = useState([]);
 
-    const [repository, setRepository] = useState({
-      reposList:[]
-    });
-  // const [isFoundRepo, setIsFoundRepo] = useState(true);
+const [loading, setLoading] = useState(false);
+const [isFound, setIsFound] = useState(true);
 
-  const [loading, setLoading] = useState(false);
-  const [isFound, setIsFound] = useState(true);
+const inputEl = useRef(null);
 
-  const inputEl = useRef(null);
+const handleEnterKey = () => {
+  const ENTER = 13;
+  if (window.event.keyCode === ENTER) {
+    search();
+    inputEl.current.blur();
+  }
+};
 
-  const handleEnterKey = () => {
-    const ENTER = 13;
-    if (window.event.keyCode === ENTER) {
-      search();
-      inputEl.current.blur();
-    }
-  };
+const selectText = () => {
+  inputEl.current.select();
+};
 
-  const selectText = () => {
-    inputEl.current.select();
-  };
+async function getApiData() {
+  const [profile] = await Promise.all([
+    fetch(`https://api.github.com/users/${inputEl.current.value}`).then(
+      (response) => response.json()
+    ),
+  ]);
+  return { profile };
+} 
 
-  async function getApiData() {
-    const [profile] = await Promise.all([
-      fetch(`https://api.github.com/users/${inputEl.current.value}`).then(
-        (response) => response.json()
-      ),
-    ]);
-    return { profile };
-  } 
-
-  async function search() {
-    setLoading(true);
-    const { profile } = await getApiData();
+async function search() {
+  setLoading(true);
+  axios({
+    method: "get",
+    url: `https://api.github.com/users/${inputEl.current.value}`,
+  }).then((res) => {
     setLoading(false);
 
-    if (profile.message === "Not Found") {
-      // inputEl.current.focus();
-      setIsFound(false);
-      setUser({
-        profile: {},
-      });
-      setRepository({
-        reposList: [],
-      });
-    } else {
-      setIsFound(true);
-      setUser({
-        profile,
-      });
-    }
+  if (user.message === "Not Found") {
+    console.log(user.message);
+    // inputEl.current.focus();
+    setIsFound(false);
+    // setUser({});
+    // setRepository([]);
+  } else {
+    setIsFound(true);
+    setUser(res.data);
   }
+});
+}
 
-  async function getApiDataRepos() {
-    const [reposList] = await Promise.all([
-      fetch(`https://api.github.com/users/${user.profile.login}/repos`).then(
-        (response) => response.json()
-      ),
-    ]);
-    
-    return { reposList };
-  }
-
-  async function OpenListRepo() {
-    // setLoading(true);
-    const { reposList } = await getApiDataRepos();
-    // setLoading(false);
-    setRepository({
-      reposList,
-    });
-  }
+async function getApiDataRepos() {
+  const [reposList] = await Promise.all([
+    fetch(`https://api.github.com/users/${user.profile.login}/repos`).then(
+      (response) => response.json()
+    ),
+  ]);
   
-  return (
-    <Container maxWidth="md">
-      <Topbar>
-        <h1>GitSearchRepo</h1>
-        <SearchBar
-          onKeyPress={handleEnterKey}
-          onFocus={selectText}
-          searchFunction={search}
-          loadingState={loading}
-          inputRef={inputEl}
+  return { reposList };
+}
+
+async function OpenListRepo() {
+  setLoading(true);
+  axios({ 
+    method: "get",
+    url: `https://api.github.com/users/${user.login}/repos`,
+  }).then((res) => {
+    setLoading(false);
+    setRepository(res.data);
+  });
+}
+
+return (
+  <Container maxWidth="md">
+    <Topbar>
+      <h1>GitSearchRepo</h1>
+      <SearchBar
+        onKeyPress={handleEnterKey}
+        onFocus={selectText}
+        searchFunction={search}
+        loadingState={loading}
+        inputRef={inputEl}
+        />
+    </Topbar>
+    {!isFound ? <h3> <center>User Tidak Ditemukan</center> </h3> : null}
+
+    {user.id && (
+          <GUserProfile 
+          data={user}
+          OpenRepoFunction={OpenListRepo}
           />
-      </Topbar>
-      {!isFound ? <h3> <center>User Tidak Ditemukan</center> </h3> : null}
+          )}
+ <Box sx={{ flexGrow: 1 }}>
+    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {repository.map((data) => (
+          <Grid item xs={2} sm={4} md={4} key={data}>
+          <RepositoryCard repo={data}></RepositoryCard>
+        </Grid>
+        ))}
+    </Grid>
+  </Box>
+  </Container>
 
-      {user.profile.id && (
-            <GUserProfile 
-            data={user.profile}
-            OpenRepoFunction={OpenListRepo}
-            />
-            )}
-   <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {repository.reposList.map((data) => (
-            <Grid item xs={2} sm={4} md={4} key={data}>
-            <RepositoryCard repo={data}></RepositoryCard>
-          </Grid>
-          ))}
-      </Grid>
-    </Box>
-    </Container>
-  
-  );
+);
 }
 
 export default App;
